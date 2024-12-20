a2bd305054b5d8c24af813d39f75ce7390b1aab1
aadicode69 <outtareach69@gmail.com>
2024-12-20 17:37:26 +0530

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <img src="https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cm9ib3R8ZW58MHx8MHx8fDA%3D" alt="imggii" className='h-fit w-fit' />
+      </div>
+    </>
+  )
+}
+
+export default App
